a33c2eb0baa870e7efa1b34edb51763deeaadaf9
Tarun <tarun27.ai@gmail.com>
2026-03-18 10:08:11 +0000

    Sync LeetCode submission Runtime - 3 ms (51.67%), Memory - 46.6 MB (24.12%)

---
+++ 2756-buy-two-chocolates/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int count=0;
+        Arrays.sort(prices);
+        if(prices[0]+prices[1]<=money)
+        {
+            return money-(prices[0]+prices[1]);
+        }
+        return money;
+    }
+}
