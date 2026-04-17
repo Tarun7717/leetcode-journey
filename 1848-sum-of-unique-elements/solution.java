e39cb8fc80ecf64779843c2a871c0be18a91aab9
Tarun <tarun27.ai@gmail.com>
2026-04-17 12:35:07 +0000

    Sync LeetCode submission Runtime - 1 ms (69.13%), Memory - 43.1 MB (35.24%)

---
+++ 1848-sum-of-unique-elements/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        int sum=0;
+        for(int j=0;j<nums.length;j++){
+            if(map.get(nums[j])==1){
+                sum+=nums[j];
+            }
+        }
+        return sum;
+    }
+}
