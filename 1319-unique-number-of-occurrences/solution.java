8ec96c0d0fcaa8b8cde8b10ee933019ccaac47b3
Tarun <tarun27.ai@gmail.com>
2026-04-18 04:42:51 +0000

    Sync LeetCode submission Runtime - 2 ms (98.25%), Memory - 43.4 MB (83.47%)

---
+++ 1319-unique-number-of-occurrences/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(set.contains(entry.getValue())){
+                return false;
+            }
+            else{
+                set.add(entry.getValue());
+            }
+        }
+        return true;
+    }
+}
