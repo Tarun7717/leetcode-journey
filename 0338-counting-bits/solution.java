ac81edaee280b1f636ec9a568cbc9aaf8e9f8677
Tarun <tarun27.ai@gmail.com>
2026-04-25 15:19:03 +0000

    Sync LeetCode submission Runtime - 9 ms (11.11%), Memory - 48.7 MB (31.05%)

---
+++ 0338-counting-bits/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] ans=new int[n+1];
+        for(int i=0;i<=n;i++){
+            int count=0;
+            String binary=Integer.toBinaryString(i);
+            char[] arr = binary.toCharArray();
+            for(char ch : arr){
+                if(ch=='1'){
+                    count++;
+                }
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+}
