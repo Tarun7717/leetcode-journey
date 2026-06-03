4b3e6515b470a393bfddceda1c144c175c8187c2
Tarun <tarun27.ai@gmail.com>
2026-06-03 14:58:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (84.60%)

--- 1013-fibonacci-number/solution.java
+++ 1013-fibonacci-number/solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)
-        {
+        
+        if(n==0){
             return 0;
         }
-        if(n==1)
-        {
-            return 1;
+        int prev=1;
+        int prev2=0;
+        int cur=0;
+        for(int i=2;i<=n;i++){
+            cur = prev+prev2;
+            prev2=prev;
+            prev=cur;
         }
-
-        return fib(n-1)+fib(n-2);
+        return prev;
     }
 }
