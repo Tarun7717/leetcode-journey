b8b536247789d18d4f66dbb5003e0c647007904d
Tarun <tarun27.ai@gmail.com>
2026-06-11 06:04:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (31.75%)

--- 0371-sum-of-two-integers/solution.java
+++ 0371-sum-of-two-integers/solution.java
@@ -1,6 +1,14 @@
 class Solution {
     public int getSum(int a, int b) {
-        
-        return a+b;
+        int carry =0;
+        int temp=0;
+        while(b!=0){
+            temp=a^b;
+            carry = a & b;
+            carry=carry<<1;
+            a=temp;
+            b=carry;
+        }
+        return a;
     }
 }
