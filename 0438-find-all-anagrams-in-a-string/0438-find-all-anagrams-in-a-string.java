0a8bc15db578c2be464c4bf984e7222be6431fe7
Tarun <tarun27.ai@gmail.com>
2026-07-02 12:22:31 +0530

    Time: 90 ms (16%), Space: 48.1 MB (6.35%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,16 +1,42 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
+        int m=s.length();
+        int n=p.length();
         List<Integer> res = new ArrayList<>();
-        char[] arr1 = p.toCharArray();
-        Arrays.sort(arr1); 
-        for(int i=0;i<s.length()-p.length()+1;i++){
-            String str = s.substring(i,i+p.length());
-            char[] arr2 = str.toCharArray();
-            Arrays.sort(arr2);
-            if(Arrays.equals(arr1,arr2)){
-                res.add(i);
+
+        if(m<n){
+            return res;
+        }
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(char ch : p.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        int l=0;
+        int r=l+n-1;
+        Map<Character,Integer> map2= new HashMap<>();
+
+            for(int i=l;i<=r;i++){
+                map2.put(s.charAt(i),map2.getOrDefault(s.charAt(i),0)+1);
             }
+            if(map.equals(map2)){
+                res.add(l);
+            }
+        while(r<m-1){
+            map2.put(s.charAt(l),map2.get(s.charAt(l))-1);
+            if(map2.get(s.charAt(l))==0){
+                map2.remove(s.charAt(l));
+            }
+            l++;
+            r++;
+            map2.put(s.charAt(r),map2.getOrDefault(s.charAt(r),0)+1);
+            if(map.equals(map2)){
+                res.add(l);
+            }
+           
         }
 
         return res;
