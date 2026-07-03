239e85c7ddcc8b14ae28c5ef7961ff8e5b63593d
Tarun <tarun27.ai@gmail.com>
2026-07-03 12:28:50 +0530

    Time: 1 ms (96.02%), Space: 43 MB (77.8%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+
+        int m=word1.length();
+        int n=word2.length();
+        
+        StringBuilder sb = new StringBuilder();
+
+        while(i<m && i<n){
+            if(m>i){
+                sb.append(word1.charAt(i));
+            }
+            if(n>i){
+                sb.append(word2.charAt(i));
+            }
+            i++;
+        }
+        while(i<n){
+            sb.append(word2.charAt(i));
+            i++;
+        }
+        while(i<m){
+            sb.append(word1.charAt(i));
+            i++;
+        }
+
+        return sb.toString();
+        
+    }
+}
