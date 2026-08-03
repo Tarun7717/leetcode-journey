2773654d31f5e1668142f22727bb3eb8fe79014f
Tarun <tarun27.ai@gmail.com>
2026-08-03 23:43:10 +0530

    Time: 0 ms (100%), Space: 46.7 MB (73.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-      
-      Set<ListNode> set = new HashSet<>();
-    
-        ListNode temp = head;
-      while(temp!=null){
-        if(set.contains(temp)){
-            return true; 
-        }
-        set.add(temp);
-        temp=temp.next;
-      }
-      return false;
+       ListNode fast = head;
+       ListNode slow = head;
+
+       while(fast!=null && fast.next!=null ){
+            fast=fast.next.next;
+            slow=slow.next;
+
+            if(fast==slow){
+                return true;
+            }
+       }
+       return false;
     }
 }
