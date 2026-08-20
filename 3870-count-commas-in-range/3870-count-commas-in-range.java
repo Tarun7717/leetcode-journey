f0f49720754252e49f9fda05ab214f615fdb1326
Tarun <tarun27.ai@gmail.com>
2026-08-20 22:48:53 +0530

    Time: 1 ms (82.96%), Space: 42.8 MB (22.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,15 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-
-        long count = 0;
-
-        long limit = 1000;
-
-        while (limit <= n) {
-            count += n - limit + 1;
-            limit *= 1000;
-        }
-
-        return (int) count;
+        if (n < 1000) return 0;
+        return n - 1000 + 1;
     }
 }
